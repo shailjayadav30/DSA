c1a67015972254cb457c16338d81017a217eb5af
shailjayadav30 <shailjayadav7275@gmail.com>
2026-09-18 23:50:39 +0530

    Implement mergesort algorithm and merge function in MergeSort class

--- SORTING/MergeSort.java
+++ SORTING/MergeSort.java
@@ -1,13 +1,59 @@
 public class MergeSort {
-    public static int[] mergesort(int arr[], int low, int high) {
-        return arr;
+    public static void mergesort(int arr[], int low, int high) {
+
+
+        if(low>=high){
+return;
+        }
+        int mid = low+(high-low) / 2;
+        mergesort(arr, low, mid);
+        mergesort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+
     }
 
-    public static void  main(String args[]) {
+    public static void merge(int arr[], int low, int mid, int high) {
+        int temp[]=new int[high-low+1];
+        int left = low;
+        int right = mid + 1;
+        int i=0;
+        while (left <= mid && right <= high) {
+            if(arr[left]<=arr[right]){
+                temp[i]=arr[left];
+                left++;
+            }
+            else{
+                temp[i]=arr[right];
+                right++;
+            }
+i++;
+        }
+    while(left<=mid){
+        temp[i]=arr[left];
+        left++;
+        i++;
+    }
+    while(right<=high){
+        temp[i]=arr[right];
+        right++;
+        i++;
+    }
+     for(i=0;i<temp.length;i++){
+        arr[low+i]=temp[i]; 
+
+     }
+
+    }
+
+    public static void main(String args[]) {
         int arr[] = { 3, 5, 1, 2, 6, 7, 8, 9, 10, 3, 4, 5 };
-        int start=0;
-        int end=arr.length/2;
-        mergesort(arr, start,end);
+        int low = 0;
+        int high = arr.length -1;
+        mergesort(arr, low, high);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i] + " ");
+        }
+
     }
 
 }
